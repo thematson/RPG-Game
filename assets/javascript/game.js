f2b309c2040ae36ff955f0ec3484eab3495bae4d
Eric Matson <matson@Erics-MacBook-Pro.local>
2017-08-31 17:28:23 -0400

    a few tweaks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,29 +4,29 @@
 			var player = [
 			{
 				name:'Forever Alone',
-				hp:110,
-				cap:30,
-				ap:15,
+				hp:115,
+				cap:20,
+				ap:16,
 				nick:'alone'
 			},
 			{
 				
 				name:'Me Gusta',
 				hp:120,
-				cap:20,
-				ap:7,
+				cap:18,
+				ap:9,
 				nick:'gusta'
 			}, 
 			{
 				name:'Trollface',
-				hp:150,
+				hp:130,
 				cap:15,
-				ap:8,
+				ap:18,
 				nick:'troll'
 			}, 
 			{
 				name:'Y U No',
-				hp:185,
+				hp:145,
 				cap:16,
 				ap:6,
 				nick:'yuno'
@@ -69,7 +69,7 @@ function reset () {
 				name:'Trollface',
 				hp:150,
 				cap:15,
-				ap:8,
+				ap:12,
 				nick:'troll'
 			}, 
 			{
@@ -100,12 +100,14 @@ function playgame() {
 			
 			var tempheroname = "";
 			var tempenemyname = "";
+			
 	//user chooses his hero	
-		$(".statbox").on("click", function() {
+		$(".statbox").on("click", function() {		 
 			if (hero == false) {
 					hero = true;
 					$(this).addClass("hero");
 					$(this).removeClass("statbox");
+					$(this).unbind("click");
 					$(this).animate({ height: "150px", width: "150px"}).css("background-color", "lightgreen");
 					$(this.id).animate({ height: "85px", width: "85px"});
 					$("#hero").append($(this)).css("color", "black");
@@ -117,7 +119,7 @@ function playgame() {
 						}
 					}
 			}
-	//user chooses his frost enemy		
+	//user chooses his first enemy		
 			else if (enemy == false) {
 					enemy = true;
 					$(this).addClass("enemy");
@@ -131,100 +133,140 @@ function playgame() {
 					$("#gang").append($(".gang")).css("color", "black");
 					$(".gang").animate({ height: "150px", width: "150px"}).css("background-color", "yellow");
 					$(".gang".id).animate({ height: "85px", width: "85px"});
-					$("#attack").append('<button type="button" class="btn btn-secondary btn-lg">Attack!</button>').css("color", "black");
+					$("#attack").html('<button type="button" class="btn btn-secondary btn-lg">Attack!</button>').css("color", "black");
 					$("#action").css("color", "black");
 					$("#fightlog").css("color", "black");
 					for (i=0; i < player.length; i++) {
 						var tempenemyname = ($(this).attr("id"));
 						if (tempenemyname == (player[i].nick)) {
 							myenemy = player[i];
-							
+														
 						}
 					}
+					
 			}
 
 			else {
 				console.log("else");
 			};
-
+			
 			fight();
 	});
 				};
 	//battle function
-function fight() {	
-					
+function fight() {		
 	//if it's the initial round, variable are assigned the original object values		 
 				if (round == 0) {
 				tempherohp = parseInt(myhero.hp);
 				}
 				var tempenemyhp = parseInt(myenemy.hp);
+				console.log(myenemy);
+				console.log(tempenemyhp);
 				var heroattack = parseInt(myhero.ap)+(round*(parseInt(myhero.ap)));
 	//button click starts melee		
 			$(".btn").on("click", function() {
-
+				console.log('clicked');
+				console.log('round ' + round);
 				heroattack = parseInt(myhero.ap)+(round*(parseInt(myhero.ap)));
+				console.log('temp hero hp is ' + tempherohp);
+				console.log('tempenemy after click is  ' + tempenemyhp);
 	//if both combatants still have health, carry on the game play					
-				if (tempherohp >= 1 && tempenemyhp >= 1) {
-				
+				if (tempherohp >= 1 && tempenemyhp >= 1) {				
 					$("#fightlog").html("<br/><br/><p>You attacked " + myenemy.name + " for " + heroattack + " damage<br/>" + 
 					myenemy.name + " attacked you back for " + myenemy.cap + " damage</p>");					
 					round++;
 					tempenemyhp=(tempenemyhp - heroattack);
-	// after a battle round, checks to see if enemy is still 'alive',
-					if (tempherohp >= 0 && tempenemyhp <= 0) {
-
-			//if not, enemy disappears and user must select another
-						$("#"+myenemy.nick).css("background-color", "black").toggle("explode").empty();	
-						setTimeout(function() {
-								$("#enemy").empty();
-								$("#enemy").html("<h4>The Enemy</h4");
-								$("#enemy").append("<br/><br/><h2>FTW!!!</h2><br/><p>Please select another opponent!</p>").fadeIn("slow");
-								$("#fightlog").empty();
-							}, 500);
-						$(".gang").on("click", function() {
-							$(".gang").unbind("click");
-							console.log(this);
-							$(this).addClass("enemy");
-							$(this).removeClass("gang");
-							$("#enemy").empty();
-							$("#enemy").html("<h4>The Enemy</h4");
-							$(this).animate({ height: "150px", width: "150px"}).css("background-color", "red");
-							$(this.id).animate({ height: "85px", width: "85px"});
-							$("#enemy").append($(".enemy")).css("color", "black");
-							for (i=0; i < player.length; i++) {
-								var tempenemyname = ($(this).attr("id"));
-								if (tempenemyname == (player[i].nick)) {
-									myenemy = player[i];
-								}
-							}
-															
-									
-						});
-					}
-
 					tempherohp=(tempherohp - parseInt(myenemy.cap));
+					console.log(tempenemyhp);
+					console.log(tempherohp);
+					
+					// if (tempenemyhp > 0) {
+						
+					
+				
+	// after a battle round, checks to see if enemy is still 'alive',
+						if (tempherohp > 0 && tempenemyhp <= 0) {
+
+					//if not, enemy disappears and user must select another
+								enemy = false;
+								// $(".gang").bind("click", handler);
+								$("#"+myenemy.nick).css("background-color", "black").toggle("explode").empty();	
+								setTimeout(function() {
+										$("#enemy").empty();
+										$("#enemy").html("<h4>The Enemy</h4");
+										$("#enemy").append("<br/><br/><h2>FTW!!!</h2><br/><p>Please select another opponent!</p>").fadeIn("slow");
+										$("#fightlog").empty();
+									}, 500);
+								$(".gang").on("click", function() {
+									$(".gang").unbind("click");
+									console.log(this);
+									console.log(player);
+									$(this).addClass("enemy");
+									$(this).removeClass("gang");
+									$("#enemy").empty();
+									$("#enemy").html("<h4>The Enemy</h4");
+									$(this).animate({ height: "150px", width: "150px"}).css("background-color", "red");
+									$(this.id).animate({ height: "85px", width: "85px"});
+									$("#enemy").append($(".enemy")).css("color", "black");
+									for (i=0; i < player.length; i++) {
+										var tempenemyname = ($(this).attr("id"));
+										if (tempenemyname == (player[i].nick)) {
+											myenemy = player[i];
+											tempenemyhp = myenemy.hp;
+
+										}
+									}																
+								});
+						}
+						if (tempherohp <= 0 && tempenemyhp > 0) {
+							playerLoses();
+						}
+						if (tempherohp <= 0 && tempenemyhp <= 0) {
+							setTimeout(function(){
+								$("body").empty();
+	
+							}, 800); 
+					}
+					// }
+				}
+				else if (tempherohp <= 0 && tempenemyhp > 0) {
+						//if not, game is over and button is clicked to reset
+						playerLoses();
+				}	
+				
+				// else if (tempherohp <= 0 && tempherohp <= 0) {
+				// 		setTimeout(function(){
+				// 			$("body").empty();
+
+				// 		}, 300); 
+				// }
+					// if (tempherohp <=0) {
+					// 	playerLoses();
+					// }
+					
 	// after battle round checks to see if user's hero is still 'alive'
-					if (tempherohp <= 0 && tempenemyhp > 0) {
-			//if not, game is over and button is clicked to reset
-						$("#"+myhero.nick).css("background-color", "black").animate({height: '300px', width: '300px', opacity: '0.4'}, "slow").toggle("explode");				
-						$(".btn").unbind("click");	
-						$(".statbox").unbind("click");				
-						$("#hero").append("<br/><br/><h2>EPIC FAIL!</h2>")
-						$("#attack").html('<button type="button" class="btn btn-success btn-lg">Restart!</button>').css("color", "black");
-						$("#fightlog").html("<br/><br/>Please click the 'Restart!' button to play again");
-						reset();
-						$(".btn").on("click", function() {
-							location.reload();
-							});
-						}	
-		//updates on-icon hit points
+	
 					$("#"+myhero.nick+"points").html("<p>" + tempherohp + "</p>");
+					console.log(tempherohp + 'written in div');
 					$("#"+myenemy.nick+"points").html("<p>" + tempenemyhp + "</p>");
 					
+				// };
+			});
+				
+					
 				};
-			});			
-			
-				};
+	function playerLoses() {
+		$("#"+myhero.nick).css("background-color", "black").animate({height: '300px', width: '300px', opacity: '0.4'}, "slow").toggle("explode");				
+		$(".btn").unbind("click");	
+		$(".statbox").unbind("click");				
+		$("#hero").append("<br/><br/><h2>EPIC FAIL!</h2>")
+		$("#attack").html('<button type="button" class="btn btn-success btn-lg">Restart!</button>').css("color", "black");
+		$("#fightlog").html("<br/><br/>Please click the 'Restart!' button to play again");
+		reset();
+		$(".btn").on("click", function() {
+			location.reload();
+			});
+	};
 //calls game to start	
 	playgame();
 	fight();
